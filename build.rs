0eb4a1ad197770d3425c43c478e87ff326b5dfe3
Tom Parker-Shemilt <palfrey@tevp.net>
2019-10-20 22:43:53 +0100

    Only enable build script if building test work

--- build.rs
+++ build.rs
@@ -1,6 +1,14 @@
 fn main() {
-    println!("cargo:rerun-if-env-changed=SQLITE_DRIVER");
-    println!("cargo:rustc-cfg=sqlite_driver=\"{}\"", std::env::var("SQLITE_DRIVER").unwrap_or("".to_string()));
-    println!("cargo:rerun-if-env-changed=POSTGRES_DRIVER");
-    println!("cargo:rustc-cfg=postgres_driver=\"{}\"", std::env::var("POSTGRES_DRIVER").unwrap_or("".to_string()));
+    if std::env::var("CARGO_CFG_TEST").is_ok() {
+        println!("cargo:rerun-if-env-changed=SQLITE_DRIVER");
+        println!(
+            "cargo:rustc-cfg=sqlite_driver=\"{}\"",
+            std::env::var("SQLITE_DRIVER").unwrap_or("".to_string())
+        );
+        println!("cargo:rerun-if-env-changed=POSTGRES_DRIVER");
+        println!(
+            "cargo:rustc-cfg=postgres_driver=\"{}\"",
+            std::env::var("POSTGRES_DRIVER").unwrap_or("".to_string())
+        );
+    }
 }

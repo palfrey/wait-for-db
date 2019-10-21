a01d7be8992d2ec4a81eb41ff7cc367e3922f952
Tom Parker-Shemilt <palfrey@tevp.net>
2019-10-21 22:27:56 +0100

    Split main into odbc/main/common

---
+++ src/common.rs
@@ -0,0 +1,40 @@
+use failure::Fail;
+use odbc::DiagnosticRecord;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "basic")]
+pub struct Opts {
+    pub connection_string: String,
+    pub sql_text: String,
+}
+
+impl Default for Opts {
+    fn default() -> Self {
+        Opts {
+            connection_string: "".to_string(),
+            sql_text: "".to_string(),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum DbErrorLifetime {
+    Permenant,
+    Temporary,
+}
+
+#[derive(Debug, Fail)]
+pub enum DbErrorType {
+    #[fail(display = "odbc error: {}", error)]
+    OdbcError { error: DiagnosticRecord },
+
+    #[fail(display = "postgres error")]
+    PostgresError,
+}
+
+#[derive(Debug)]
+pub struct DbError {
+    pub kind: DbErrorLifetime,
+    pub error: DbErrorType,
+}
---
+++ src/odbc.rs
@@ -0,0 +1,148 @@
+use odbc::{create_environment_v3, Connection, Data, DiagnosticRecord, NoData, Statement};
+use std::ffi::CStr;
+
+use crate::common::{DbError, DbErrorLifetime, DbErrorType, Opts};
+
+impl From<DiagnosticRecord> for DbError {
+    #[rustfmt::skip]
+    fn from(item: DiagnosticRecord) -> Self {
+        let state = CStr::from_bytes_with_nul(item.get_raw_state())
+            .unwrap()
+            .to_str()
+            .unwrap();
+        let kind = if state == "IM002"  // no driver
+                    || state == "01000" // no driver at path
+                    || state == "IM004" // bad driver
+                    || state == "08001" // can't connect
+        {
+            DbErrorLifetime::Permenant
+        } else {
+            DbErrorLifetime::Temporary
+        };
+        DbError {
+            kind: kind,
+            error: DbErrorType::OdbcError { error: item },
+        }
+    }
+}
+
+pub fn connect(opts: Opts) -> std::result::Result<(), DbError> {
+    let env = create_environment_v3().map_err(|e| e.unwrap())?;
+    let conn = env.connect_with_connection_string(&opts.connection_string)?;
+    execute_statement(&conn, opts)
+}
+
+fn execute_statement<'env>(conn: &Connection<'env>, opts: Opts) -> Result<(), DbError> {
+    let stmt = Statement::with_parent(conn)?;
+
+    match stmt.exec_direct(&opts.sql_text)? {
+        Data(mut stmt) => {
+            let cols = stmt.num_result_cols()?;
+            while let Some(mut cursor) = stmt.fetch()? {
+                for i in 1..(cols + 1) {
+                    match cursor.get_data::<&str>(i as u16)? {
+                        Some(val) => print!(" {}", val),
+                        None => print!(" NULL"),
+                    }
+                }
+                println!("");
+            }
+        }
+        NoData(_) => println!("Query executed, no data returned"),
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_connect_no_driver() {
+        let err = connect(Opts::default()).unwrap_err();
+        assert_eq!(err.kind, DbErrorLifetime::Permenant, "{:?}", err);
+    }
+
+    #[test]
+    fn test_connect_with_missing_driver() {
+        let err = connect(Opts {
+            connection_string: "Driver=foo;Server=blah;".to_string(),
+            sql_text: "".to_string(),
+        })
+        .unwrap_err();
+        assert_eq!(err.kind, DbErrorLifetime::Permenant, "{:?}", err);
+        if let DbErrorType::OdbcError { error } = err.error {
+            let desc = format!("{}", error);
+            assert!(desc.contains("Can't open lib 'foo' : file not found"), desc);
+        }
+    }
+
+    #[test]
+    fn test_connect_with_bad_driver() {
+        let err = connect(Opts {
+            connection_string: format!(
+                "Driver={};Server=blah;",
+                std::env::current_exe().unwrap().to_str().unwrap()
+            ),
+            sql_text: "".to_string(),
+        })
+        .unwrap_err();
+        assert_eq!(err.kind, DbErrorLifetime::Permenant, "{:?}", err);
+        if let DbErrorType::OdbcError { error } = err.error {
+            let desc = format!("{}", error);
+            assert!(
+                desc.contains("Driver's SQLAllocHandle on SQL_HANDLE_HENV failed"),
+                desc
+            );
+        }
+    }
+
+    #[test]
+    #[cfg_attr(sqlite_driver = "", ignore)]
+    fn test_sqlite_with_good_driver() {
+        connect(Opts {
+            connection_string: format!(
+                "Driver={};Database=test.db;",
+                std::env::var("SQLITE_DRIVER").unwrap()
+            ),
+            sql_text: "SELECT 1 from sqlite_master".to_string(),
+        })
+        .unwrap();
+    }
+
+    #[test]
+    #[cfg_attr(postgres_driver = "", ignore)]
+    fn test_postgres_with_no_server() {
+        let err = connect(Opts {
+            connection_string: format!("Driver={};", std::env::var("POSTGRES_DRIVER").unwrap()),
+            sql_text: "".to_string(),
+        })
+        .unwrap_err();
+        assert_eq!(err.kind, DbErrorLifetime::Permenant, "{:?}", err);
+        if let DbErrorType::OdbcError { error } = err.error {
+            let desc = format!("{}", error);
+            assert!(
+                desc.contains("could not connect to server: No such file or directory"),
+                desc
+            );
+        }
+    }
+
+    #[test]
+    #[cfg_attr(postgres_driver = "", ignore)]
+    fn test_postgres_with_server() {
+        connect(Opts {
+            connection_string: format!(
+                "Driver={};Server={};Port={};Uid={};Pwd={};",
+                std::env::var("POSTGRES_DRIVER").unwrap(),
+                std::env::var("POSTGRES_SERVER").unwrap(),
+                std::env::var("POSTGRES_PORT").unwrap(),
+                std::env::var("POSTGRES_USERNAME").unwrap(),
+                std::env::var("POSTGRES_PASSWORD").unwrap(),
+            ),
+            sql_text: "SHOW config_file".to_string(),
+        })
+        .unwrap();
+    }
+}

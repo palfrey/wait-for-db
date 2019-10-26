fd6c3bcab20481d13e89b68f88c46c9d6c178036
Tom Parker-Shemilt <palfrey@tevp.net>
2019-10-26 21:26:22 +0100

    CARGO_CFG_TEST is never set (see https://github.com/rust-lang/cargo/issues/4789) so always set the env variable changers

--- build.rs
+++ build.rs
@@ -1,14 +1,12 @@
 fn main() {
-    if std::env::var("CARGO_CFG_TEST").is_ok() {
-        println!("cargo:rerun-if-env-changed=SQLITE_DRIVER");
-        println!(
-            "cargo:rustc-cfg=sqlite_driver=\"{}\"",
-            std::env::var("SQLITE_DRIVER").unwrap_or("".to_string())
-        );
-        println!("cargo:rerun-if-env-changed=POSTGRES_DRIVER");
-        println!(
-            "cargo:rustc-cfg=postgres_driver=\"{}\"",
-            std::env::var("POSTGRES_DRIVER").unwrap_or("".to_string())
-        );
-    }
+    println!("cargo:rerun-if-env-changed=SQLITE_DRIVER");
+    println!(
+        "cargo:rustc-cfg=sqlite_driver=\"{}\"",
+        std::env::var("SQLITE_DRIVER").unwrap_or("".to_string())
+    );
+    println!("cargo:rerun-if-env-changed=POSTGRES_DRIVER");
+    println!(
+        "cargo:rustc-cfg=postgres_driver=\"{}\"",
+        std::env::var("POSTGRES_DRIVER").unwrap_or("".to_string())
+    );
 }

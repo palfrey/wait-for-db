a63277e0061fd29af43ff198360f5046d95e2798
Tom Parker-Shemilt <palfrey@tevp.net>
2025-11-23 18:42:59 +0000

    Strip down the features in the dependencies a lot

--- build.rs
+++ build.rs
@@ -1,13 +1,15 @@
 fn main() {
     // Env variables here are only actually used by the tests
-    println!("cargo:rerun-if-env-changed=SQLITE_DRIVER");
+    println!("cargo::rerun-if-env-changed=SQLITE_DRIVER");
+    println!("cargo::rustc-check-cfg=cfg(sqlite_driver, values(\"\"))");
     println!(
-        "cargo:rustc-cfg=sqlite_driver=\"{}\"",
+        "cargo::rustc-cfg=sqlite_driver=\"{}\"",
         std::env::var("SQLITE_DRIVER").unwrap_or_else(|_| "".to_string())
     );
-    println!("cargo:rerun-if-env-changed=POSTGRES_DRIVER");
+    println!("cargo::rerun-if-env-changed=POSTGRES_DRIVER");
+    println!("cargo::rustc-check-cfg=cfg(postgres_driver, values(\"\"))");
     println!(
-        "cargo:rustc-cfg=postgres_driver=\"{}\"",
+        "cargo::rustc-cfg=postgres_driver=\"{}\"",
         std::env::var("POSTGRES_DRIVER").unwrap_or_else(|_| "".to_string())
     );
 }
